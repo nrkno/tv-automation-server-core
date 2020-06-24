daf90dd9355f0d76e9280edaaaafd35058a7f32d
Jan Starzak <jan.starzak@gmail.com>
2020-06-24 14:50:18 +0200

    feat: shelf url display options

--- meteor/client/ui/RundownView.tsx
+++ meteor/client/ui/RundownView.tsx
@@ -1292,6 +1292,10 @@ interface ITrackedProps {
 	buckets: Bucket[]
 	casparCGPlayoutDevices?: PeripheralDevice[]
 	rundownLayoutId?: RundownLayoutId
+	shelfDisplayOptions: {
+		buckets: boolean
+		layout: boolean
+	}
 }
 export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((props: IProps) => {
 	let playlistId
@@ -1310,6 +1314,8 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 	}
 
 	const params = queryStringParse(location.search)
+
+	const displayOptions = ((params['display'] as string) || 'buckets,layout').split(',')
 
 	// let rundownDurations = calculateDurations(rundown, parts)
 	return {
@@ -1355,6 +1361,10 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				}).fetch()) ||
 			undefined,
 		rundownLayoutId: protectString((params['layout'] as string) || ''),
+		shelfDisplayOptions: {
+			buckets: displayOptions.includes('buckets'),
+			layout: displayOptions.includes('layout'),
+		},
 	}
 })(
 	class RundownView extends MeteorReactComponent<Translated<IProps & ITrackedProps>, IState> {
@@ -1374,7 +1384,6 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 			label: string
 			global?: boolean
 		}> = []
-		private _inspectorShelf: WrappedShelf | null
 		private _segmentZoomOn: boolean = false
 
 		constructor(props: Translated<IProps & ITrackedProps>) {
@@ -2223,10 +2232,6 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 			})
 		}
 
-		setInspectorShelf = (isp: WrappedShelf | null) => {
-			this._inspectorShelf = isp
-		}
-
 		onTake = (e: any) => {
 			const { t } = this.props
 			if (this.state.studioMode && this.props.playlist) {
@@ -2393,7 +2398,6 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 								</ErrorBoundary>
 								<ErrorBoundary>
 									<Shelf
-										ref={this.setInspectorShelf}
 										buckets={this.props.buckets}
 										isExpanded={this.state.isInspectorShelfExpanded}
 										onChangeExpanded={this.onShelfChangeExpanded}
@@ -2404,6 +2408,7 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 										onChangeBottomMargin={this.onChangeBottomMargin}
 										onRegisterHotkeys={this.onRegisterHotkeys}
 										rundownLayout={this.state.rundownLayout}
+										shelfDisplayOptions={this.props.shelfDisplayOptions}
 									/>
 								</ErrorBoundary>
 								<ErrorBoundary>
@@ -2435,7 +2440,6 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 					return (
 						<ErrorBoundary>
 							<Shelf
-								ref={this.setInspectorShelf}
 								buckets={this.props.buckets}
 								isExpanded={this.state.isInspectorShelfExpanded}
 								onChangeExpanded={this.onShelfChangeExpanded}
@@ -2447,6 +2451,7 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 								onRegisterHotkeys={this.onRegisterHotkeys}
 								rundownLayout={this.state.rundownLayout}
 								fullViewport={true}
+								shelfDisplayOptions={this.props.shelfDisplayOptions}
 							/>
 						</ErrorBoundary>
 					)
